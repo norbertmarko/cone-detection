97013b761e9644fd6c0e8f26dacc6e3e3fbcf0de
norbertmarko <norbertmarko92@gmail.com>
2021-07-05 14:21:05 +0200

    Function structure.

--- src/distance.py
+++ src/distance.py
@@ -1,38 +1,54 @@
 import numpy as np
 import cv2
+import os
 import time
 import sys
-import os
+from numba import jit
 
-num = 0
-path = "./meresek/rec0/"
-if (len(sys.argv) == 2):
-    path = sys.argv[1]
+def read_npy_file(path, idx):
+    img_depth = np.load(os.path.join(path, "frame_depth_" + str(idx) + ".npy"))
+    img_color = np.load(os.path.join(path, "frame_color_" + str(idx) + ".npy"))
+    return (img_depth, img_color)
 
-sleep_time = 0.01
 
-try:
-    while (True):
-        depth_image = np.load(os.path.join(path, "frame_depth_" + str(num) + ".npy"))
-        color_image = np.load(os.path.join(path, "frame_color_" + str(num) + ".npy"))
+def play_video_from_npy(path):
+    """
+    Play numpy array files along with the chosen preprocessing.
+    Shapes:
+        img_color: (480, 640, 3), dtype: np.uint8
+        img_depth: (480, 640), dtpye: np.uint16
+        colormap_depth: (480, 640, 3), dtype: np.uint8
+    """
+    datatype = np.uint8
+    sleep_time = 0.01
+    idx = 0
+    try:
+        while(True):
+            (img_depth, img_color) = read_npy_file(path, idx)
+            
+            idx += 1
 
-        print("Color: {}".format(color_image.shape))
-        print("Depth: {}".format(depth_image.shape))
+            colormap_depth = apply_colormap(img_depth)
+            print("Depth: {}".format(colormap_depth.shape))
 
-        num = num + 1
+            #* Put processing function here.
 
-        #* Put processing function here.
+            # display
+            cv2.imshow("color_image", img_color.astype(datatype))
+            cv2.imshow("depth_image", colormap_depth.astype(datatype))
+            
+            # required for proper display
+            if cv2.waitKey(1) == ord('q'): break
+            time.sleep(sleep_time)           
+    except:
+        print("[INFO] Video sequence end.")
+    finally:
+        cv2.destroyAllWindows()
 
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), 9)
 
-        cv2.imshow("color_image", color_image.astype(np.uint8))
-        cv2.imshow("depth_image", depth_colormap)
-        
-        if cv2.waitKey(1) == ord('q'):
-            break
+def apply_colormap(img_depth):
+    return cv2.applyColorMap(cv2.convertScaleAbs(img_depth, alpha=0.03), 9)
 
-        time.sleep(sleep_time)
-except:
-    print("[INFO] Video sequence end.")
-finally:
-    cv2.destroyAllWindows() 
+if __name__ == '__main__':
+    path = "./meresek/rec0/"
+    play_video_from_npy(path)
